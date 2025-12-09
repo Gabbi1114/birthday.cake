3321c8b21ea94d6507f289693e928b9bce10bc79
Gabbi1114 <gabi_huwow@yahoo.com>
2025-12-09 16:45:05 +0800

    Code formatting: update formatting in PartyHat component

--- components/PartyHat.tsx
+++ components/PartyHat.tsx
@@ -30,45 +30,28 @@ const PartyHat: React.FC<PartyHatProps> = ({
       {/* Cone hat body */}
       <mesh castShadow receiveShadow rotation={[0, 0, 0]}>
         <coneGeometry args={[1.5, 4, 16]} />
-        <meshStandardMaterial
-          color={color}
-          roughness={0.3}
-          metalness={0.1}
-        />
+        <meshStandardMaterial color={color} roughness={0.3} metalness={0.1} />
       </mesh>
 
       {/* Pom-pom on top */}
       <mesh position={[0, 2.2, 0]} castShadow>
         <sphereGeometry args={[0.3, 16, 16]} />
-        <meshStandardMaterial
-          color="#FFFFFF"
-          roughness={0.2}
-          metalness={0.2}
-        />
+        <meshStandardMaterial color="#FFFFFF" roughness={0.2} metalness={0.2} />
       </mesh>
 
       {/* Decorative stripe */}
       <mesh position={[0, 0.5, 0]} rotation={[0, 0, 0]}>
         <torusGeometry args={[1.3, 0.15, 8, 16]} />
-        <meshStandardMaterial
-          color="#FFD700"
-          roughness={0.2}
-          metalness={0.3}
-        />
+        <meshStandardMaterial color="#FFD700" roughness={0.2} metalness={0.3} />
       </mesh>
 
       {/* Bottom rim */}
       <mesh position={[0, -2, 0]} rotation={[0, 0, 0]}>
         <torusGeometry args={[1.5, 0.2, 8, 16]} />
-        <meshStandardMaterial
-          color="#FFFFFF"
-          roughness={0.3}
-          metalness={0.1}
-        />
+        <meshStandardMaterial color="#FFFFFF" roughness={0.3} metalness={0.1} />
       </mesh>
     </group>
   );
 };
 
 export default PartyHat;
-
