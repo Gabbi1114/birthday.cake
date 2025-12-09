eedfa0555b5b6fe4a0383c86c9da43fc3b8b69d6
Gabbi1114 <gabi_huwow@yahoo.com>
2025-12-09 17:01:25 +0800

    Adjust party hat position to sit on top of table cylinder surface

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -310,7 +310,7 @@ const Scene: React.FC<SceneProps> = ({
         </group>
 
         {/* Party Hat - on table in bottom right area (red circle area) */}
-        {/* Position: x=12, z=12 (bottom right), y=-5.6 (bottom rim at -7.6 table surface) */}
+        {/* Position: x=12, z=12 (bottom right), y=-5.6 (bottom rim at -7.6 sits on table top) */}
         <PartyHat position={[12, -5.6, 12]} color="#FF6B6B" scale={2.5} />
 
         <Formation>
